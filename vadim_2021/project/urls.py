d45291c5fb24c81d664e13049fd3196a7efb800c
Vadimm100 <vadimnamber1@mail.ru>
2023-02-03 10:53:31 +0300

    start_files

--- vadim_2021/project/urls.py
+++ vadim_2021/project/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('project/', views.project),
+    path("", views.project_index, name="project_index"),
+    path("<int:pk>/", views.project_detail, name="project_detail"),
 ]
